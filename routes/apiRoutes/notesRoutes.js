4cfb6c5f5272ed89a34026fcb9b114093bcaa0e1
vhivestate <garcia.valeria001@gmail.com>
2022-06-17 20:23:46 -0600

    added notes routes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,17 +1,13 @@
 const router = require('express').Router();
-const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
-const { animals } = require('../../data/animals');
+const { findById, createNote, validateNotes, deleteNote } = require('../../lib/notes');
+const { animals } = require('../../db/db.json');
 
-router.get('/animals', (req, res) => {
-  let results = animals;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
+router.get('/notes', (req, res) => {
+  res.json(notes);
 });
 
-router.get('/animals/:id', (req, res) => {
-  const result = findById(req.params.id, animals);
+router.get('/notes/:id', (req, res) => {
+  const result = findById(req.params.id, notes);
   if (result) {
     res.json(result);
   } else {
@@ -19,15 +15,15 @@ router.get('/animals/:id', (req, res) => {
   }
 });
 
-router.post('/animals', (req, res) => {
+router.post('/notes', (req, res) => {
   // set id based on what the next index of the array will be
-  req.body.id = animals.length.toString();
+  req.body.id = notes.length.toString();
 
-  if (!validateAnimal(req.body)) {
-    res.status(400).send('The animal is not properly formatted.');
+  if (!validateNotes(req.body)) {
+    res.status(400).send('Note is not properly formatted.');
   } else {
-    const animal = createNewAnimal(req.body, animals);
-    res.json(animal);
+    const note = createNote(req.body, notes);
+    res.json(note);
   }
 });
 
