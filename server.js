a85c9b8bc0156479acdba48c6592048d356ceaa1
vhivestate <garcia.valeria001@gmail.com>
2022-06-17 19:14:45 -0600

    added notes.js

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const express = require('express');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
 // Use apiRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
